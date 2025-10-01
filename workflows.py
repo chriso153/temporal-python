56b16caf1b14fe84c155e97d11444d7e71ceda0d
Chris Oman <chriso153@gmail.com>
2025-10-01 13:36:13 -0400

    Pass through the activity to the sandbox

--- workflows.py
+++ workflows.py
@@ -1,6 +1,10 @@
 from datetime import timedelta
 from temporalio import workflow
-from activities import say_hello
+
+# We know that our activity is deterministic, so we can import the activity,
+# passing it through the sandbox without reloading the module
+with workflow.unsafe.imports_passed_through():
+    from activities import say_hello
 
 @workflow.defn
 class SayHello:
